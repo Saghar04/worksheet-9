1fc1f6473ed7e8bed8e662ff4c16e31409c62521
Saghar Salamat <salamatsaghar535@gmail.com>
2023-12-11 16:55:10 +0100

    my first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 11 16:46:01 2023
+
+@author: salam
+"""
+
+import supportfile
+
+supportfile.saysomething()
+
+print('hello worldq')
---
+++ supportfile.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 11 16:42:00 2023
+
+@author: salam
+"""
+
+def saysomething():
+    print('something')
